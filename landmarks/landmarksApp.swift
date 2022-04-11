09b7512408ba73cabf5582e1fbc1c8ddfd33d418
ptone545 <payten.tyler@gmail.com>
2022-04-11 08:56:09 -0600

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Payten O'Driscoll on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
