1d0adec9aadd8bbb61c1f22fd8a598ee971d5d62
payten <payten.tyler@gmail.com>
2022-09-23 19:15:35 -0600

    DrawingPathsAndShapes

---
+++ DrawingPathsAndShapes/StartingPoint/Landmarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Payten O'Driscoll on 9/23/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
